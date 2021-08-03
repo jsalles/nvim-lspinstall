af8b3cc3a86d713b661adf9d783603ea67dcb97b
Joao Salles <joao.salles@check24.de>
2021-08-03 12:48:51 +0200

    Use npm package for stylelint

--- lua/lspinstall/servers/stylelint.lua
+++ lua/lspinstall/servers/stylelint.lua
@@ -1,24 +1,9 @@
 local config = require("lspinstall/util").extract_config("stylelint_lsp")
-config.default_config.cmd[1] = "./stylelint_lsp/dist/index.js"
+config.default_config.cmd[1] = "./node_modules/.bin/stylelint-lsp"
 
 return vim.tbl_extend("error", config, {
 	install_script = [[
-  curl -o stylelint.tar.gz -L https://github.com/bmatcuk/stylelint-lsp/archive/refs/tags/v1.2.4.tar.gz
-  rm -rf stylelint
-  mkdir stylelint
-  tar -xzf stylelint.tar.gz -C stylelint --strip-components 1
-  rm stylelint.tar.gz
-
-  cd stylelint
-  npm install
-  npm run build
-
-  cd ..
-  rm -rf stylelint_lsp
-  mkdir stylelint_lsp
-  cp -r stylelint/node_modules stylelint_lsp
-  cp -r stylelint/dist stylelint_lsp
-
-  rm -rf stylelint
+  ! test -f package.json && npm init -y scope=lspinstall || true
+  npm install stylelint-lsp
   ]],
 })
